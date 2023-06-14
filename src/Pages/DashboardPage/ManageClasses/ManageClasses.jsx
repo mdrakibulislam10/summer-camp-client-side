4fcb4da79cdb8fa305e4dcb770efd314449643d2
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-06-14 17:35:51 +0600

    send feedback for class by admin

--- src/Pages/DashboardPage/ManageClasses/ManageClasses.jsx
+++ src/Pages/DashboardPage/ManageClasses/ManageClasses.jsx
@@ -1,9 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import SectionTItle from "../../../components/SectionTItle/SectionTItle";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 // load all classes
 const ManageClasses = () => {
+    const [feedbackText, setFeedbackText] = useState("");
+    const [selectedClassId, setSelectedClassId] = useState(null);
+
     const { data: classes = [], refetch } = useQuery({
         queryKey: ["classes"],
         queryFn: async () => {
@@ -19,6 +25,25 @@ const ManageClasses = () => {
             .then(res => {
                 if (res.data.modifiedCount) {
                     refetch();
+                }
+            })
+    };
+
+    // send feedback
+    const sendFeedback = () => {
+        if (!selectedClassId || !feedbackText) {
+            return;
+        }
+
+        // console.log(feedbackText);
+
+        axios.patch(`http://localhost:5000/classes/feedback/${selectedClassId}`, { feedbackText })
+            .then(res => {
+                if (res.data.modifiedCount) {
+                    console.log(res.data.modifiedCount);
+                    toast("Feedback has been sent");
+                    refetch();
+                    setFeedbackText("");
                 }
             })
     };
@@ -63,25 +88,46 @@ const ManageClasses = () => {
                                             <td>{martialClass?.status}</td>
                                             <td>
                                                 <button disabled={martialClass.status === "approved" || martialClass.status === "denied"}
-
                                                     onClick={() => handleStatusUp(martialClass._id, "approved")}
                                                     className="btn btn-xs bg-green-400 text-white hover:text-black hover:bg-gray-200">Approve</button>
                                             </td>
                                             <td>
                                                 <button disabled={martialClass.status === "approved" || martialClass.status === "denied"}
-
                                                     onClick={() => handleStatusUp(martialClass._id, "denied")}
                                                     className="btn btn-xs bg-red-400 text-white hover:text-black hover:bg-gray-200">Deny</button>
                                             </td>
                                             <td>
-                                                <button disabled={martialClass.role === "admin"} className="btn btn-xs bg-orange-500 text-white hover:text-black hover:bg-gray-200">Send Feedback</button>
+                                                <button onClick={() => {
+                                                    setSelectedClassId(martialClass._id);
+                                                    window.my_modal_5.showModal();
+                                                }}
+                                                    className="btn btn-xs bg-orange-500 text-white hover:text-black hover:bg-gray-200">Send Feedback</button>
                                             </td>
                                         </tr>
                                     )
                                 }
+                                <ToastContainer />
                             </tbody>
 
                         </table>
+
+                        {/* Modal */}
+                        <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                            <form method="dialog" className="modal-box">
+                                <h3 className="font-bold text-lg">Write feedback</h3>
+                                <div className="form-control">
+                                    <textarea
+                                        value={feedbackText}
+                                        onChange={(e) => setFeedbackText(e.target.value)}
+                                        className="textarea textarea-bordered h-24 mt-5" placeholder="write here...">
+                                    </textarea>
+                                </div>
+                                <div className="modal-action">
+                                    <button className="btn" onClick={() => setFeedbackText("")}>Close</button>
+                                    <button className="btn" onClick={sendFeedback}>Send</button>
+                                </div>
+                            </form>
+                        </dialog>
                     </div>
                 </div>
             </section>
